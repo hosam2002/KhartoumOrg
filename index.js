5f9d0d35b39a1ca45c06621c943f38d4e2701304
iamhosam <umbaddi90@gmail.com>
2024-02-16 09:50:15 +0200

    changing the memberships cards style

--- index.js
+++ index.js
@@ -29,3 +29,55 @@ disable.addEventListener(`click`, (e) => {
     disable.style.width = "0"
     checkbox.checked = false
 })
+
+/* ============ Expandable Cards ============ */
+
+/* ========= variables ========= */
+
+const container = document.getElementById(`container`)
+const cards = document.getElementById(`cards`)
+
+/* ========= event listeners ========= */
+
+cards.addEventListener(`click`, (e) => {
+
+    const elementID = e.target.id;
+
+    if (elementID == `card-expand-0` || 
+        elementID == `card-expand-1` || 
+        elementID == `card-expand-2` || 
+        elementID == `card-expand-3` ||
+        elementID == `card-expand-4`) {
+
+        /* ========= variables ========= */
+
+        const arr   = e.target.id.split("-")
+        const id    = parseInt(arr[arr.length - 1])
+        const body  = document.getElementById(`body-${id}`)
+        const event = document.getElementById(`card-expand-${id}`)
+        const dropdown = document.getElementById(`down-${id}`)
+
+        /* ========= enable dropdown ========= */
+
+        if (event.checked == true) {
+            
+            body.style.height = `auto`;
+            dropdown.style.display = `block`
+
+            setTimeout(() => {}, 400)
+
+        } else {
+            
+            body.style.height = `12rem`
+            dropdown.style.display = `none`
+        }
+
+        /* ========= disable dropdown ========= */
+
+        dropdown.addEventListener(`click`, () => {
+
+            body.style.height = `12rem`
+            dropdown.style.display = `none`
+        })
+    }
+})
